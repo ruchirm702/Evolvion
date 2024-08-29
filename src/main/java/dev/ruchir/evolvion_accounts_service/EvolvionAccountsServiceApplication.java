b04d48068371bddac8b5b2411d29c6aae8bf4ea6
Ruchir Mandora <ruchirm702@gmail.com>
2024-08-29 22:33:09 +0530

    Initial commit with base project setup

---
+++ src/main/java/dev/ruchir/evolvion_accounts_service/EvolvionAccountsServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_accounts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolvionAccountsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvolvionAccountsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ruchir/evolvion_accounts_service/EvolvionAccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_accounts_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolvionAccountsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
